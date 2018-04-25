06222a209af7b817ea5e7a9520455dbd8db03a46
Yungon Park <hahafree12@gmail.com>
2018-04-25 22:26:44 +0900

    결과 assert 하도록 테스트 코드 변경

--- test/test_MovieParser.py
+++ test/test_MovieParser.py
@@ -10,6 +10,8 @@ class TestMovieParser(unittest.TestCase):
         schedules = cj_parser.get_channel_schedule('http://ocn.tving.com/ocn/schedule')
         print(schedules)
 
+        self.assertNotEqual(schedules, None)
+
     def test_parse_tcast_channel(self):
         tcast_parser = parser.TCastScheduleParser('Screen')
         end_date, schedules = tcast_parser.get_channel_schedule('http://www.imtcast.com/screen/program/schedule.jsp')
@@ -18,3 +20,7 @@ class TestMovieParser(unittest.TestCase):
 
         if os.path.exists('/tmp/ghostdriver.log'):
             os.remove('/tmp/ghostdriver.log')
+
+        self.assertNotEqual(end_date, None)
+        self.assertNotEqual(schedules, None)
+        self.assertEqual(os.path.exists('/tmp/ghostdriver.log'), False)
