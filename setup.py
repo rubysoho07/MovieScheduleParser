e1f93190c78f254b616619cb5352ba7bd4955e6a
Yungon Park <hahafree12@gmail.com>
2020-08-15 19:38:08 +0900

    0.0.3 버전으로 업데이트

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='MovieScheduleParser',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/rubysoho07/MovieScheduleParser',
     author='Yungon Park',
     author_email='hahafree12@gmail.com',
     description='Parsing broadcasting schedules from Korean cable broadcasting stations.',
     install_requires=[
-        "beautifulsoup4 == 4.8.0",
-        "requests == 2.20.0",
-        "selenium == 3.141.0"
+        "beautifulsoup4 >= 4.9.1",
+        "requests >= 2.24.0",
+        "selenium >= 3.141.0"
     ],
     packages=find_packages(
         exclude=['test']
